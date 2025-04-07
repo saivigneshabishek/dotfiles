b41759762dcfd675d33fa23669260230dbf6f72d
Sai Vignesh Abishek <saivigneshabishek@gmail.com>
2025-04-07 14:43:13 +0200

    add vim init config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,6 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#888888" })

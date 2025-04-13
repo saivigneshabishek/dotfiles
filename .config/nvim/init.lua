bc7fe964e9d75c40989cf1be74a8d2bab3f0ac67
Sai Vignesh Abishek <saivigneshabishek@gmail.com>
2025-04-13 02:12:59 +0200

    fix stupid indents

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,72 +14,72 @@ vim.g.mapleader = " "
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 
-	{
-		"catppuccin/nvim",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				integrations = {
-					treesitter = true,
-				},
-			})
-			vim.cmd([[colorscheme catppuccin]])
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-			vim.api.nvim_set_hl(0, "LineNr", { fg = "#fddcf7" })
-			vim.api.nvim_set_hl(0, "Comment", { fg = "#9d9d9d" })
-		end,
+    {
+        "catppuccin/nvim",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+                integrations = {
+                    treesitter = true,
+                },
+            })
+            vim.cmd([[colorscheme catppuccin]])
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+            vim.api.nvim_set_hl(0, "LineNr", { fg = "#fddcf7" })
+            vim.api.nvim_set_hl(0, "Comment", { fg = "#9d9d9d" })
+        end,
 
-	},
-    
-	{
-		"nvim-treesitter/nvim-treesitter",
-		priority = 1000,
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "python", "rust", "c", "cpp", "html", "css", "lua", "bash", "json", "markdown" },
-				auto_install = true,
-				sync_install = false,
-				indent = { enable = true },
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false
-				},
-			})
-		end,
-	},
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        priority = 1000,
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "python", "rust", "c", "cpp", "html", "css", "lua", "bash", "json", "markdown" },
+                auto_install = true,
+                sync_install = false,
+                indent = { enable = true },
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false
+                },
+            })
+        end,
+    },
 
     {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.8",
         dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
         config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-			vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Telescope live grep" })
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+            vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Telescope live grep" })
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
             vim.keymap.set("n", "<leader>q", vim.cmd.bd, { desc = "Close buffer" })
             vim.keymap.set("n", "<leader>j", vim.cmd.bnext, { desc = "Next buffer" })
             vim.keymap.set("n", "<leader>k", vim.cmd.bprev, { desc = "Prev buffer" })
-		end,
-	},
+        end,
+    },
 })
